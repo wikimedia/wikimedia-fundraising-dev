0b6b36ef38242207a5c954af1d13ce898b13ad56
Elliott Eggleston <ejegg@ejegg.com>
2021-04-30 16:58:27 -0500

    Delete drupal settings override
    This is conflicting with something in the buildkit config which
    we actually want.
    
    We need to merge the buildkit patch to create the fredge db or
    this will cause errors.
    
    Bug: T281609
    Depends-On: Ibfbb362ee2eda071112804bc28f61c40e111a8d5
    Change-Id: Icd0e5ef0ede687b9d5a06b572417b43b5fb11c73

--- config/civicrm/drupal-settings.d/50-wmff.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $databases;
-
-$databases['donations']['default'] = $databases['default']['default'];
-$databases['fredge']['default'] = $databases['default']['default'];
-
-define('WMF_SOURCE_REVISION', 'integration');
