be1562ded96f0ebc25041964c9961e38c69ee0a7
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-06-11 15:06:41 +1200

    Add php-ide-config
    This allows routing of drush calls - eg. if you have
    a project with wmff configured as a deployment
    it will connect to it when you run drush (if your
    ide is listening & you have OK breakpoints
    
    Change-Id: Icbf847e53bac3866a5a5cd4341941279726d14be

--- config/civicrm/drush/site-aliases/aliases.drushrc.php
+++ config/civicrm/drush/site-aliases/aliases.drushrc.php
@@ -2,8 +2,20 @@
 $aliases['wmff'] = [
   'root' => '/srv/civi-sites/wmff/drupal',
   'uri' => 'https://wmff.localhost:32353/',
+  '#env-vars' => [
+    // IDE call back - if you call your deployment config
+    // wmff in phpstorm this will connect to it
+    // when using drush.
+    'PHP_IDE_CONFIG' => 'serverName=wmff',
+  ],
 ];
 $aliases['dmaster'] = [
   'root' => '/srv/civi-sites/dmaster/web',
   'uri' => 'https://dmaster.localhost:32353/',
+  '#env-vars' => [
+    // IDE call back - if you call your deployment config
+    // dmaster in phpstorm this will connect to it
+    // when using drush.
+    'PHP_IDE_CONFIG' => 'serverName=dmaster',
+  ],
 ];
