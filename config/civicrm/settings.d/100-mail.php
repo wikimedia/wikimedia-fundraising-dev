d3f7b01d34697779c3c264d579b1eb1bfea94df1
cstone <cstone@wikimedia.org>
2021-08-13 18:42:29 -0400

    Add mailcatcher
    In the civi UI Administer->System Settings->Outbound Email
    SMTP Server = mailcatcher
    SMTP Port = 1025 ( or the new one you picked)
    
    Then http://localhost:1080 (or the new mailcatcher port you picked)
    
    Change-Id: I15a0dec52cd6fa131940961cb310c5aa5a602796

---
+++ config/civicrm/settings.d/100-mail.php
@@ -0,0 +1,9 @@
+<?php
+
+// This overrides the file in civicrm-buildkit/app/civicrm.settings.d/100-mail.php to
+// send email to mailcatcher. When CIVICRM_MAIL_LOG is defined all email will go to
+// that folder
+
+//if (!defined('CIVICRM_MAIL_LOG')) {
+  //define('CIVICRM_MAIL_LOG', '/dev/null');
+//}
