eb19df6fbc6eaddf34a42f999e313e4396f7cd43
Andrew Green <andrew.green.df@gmail.com>
2021-10-17 10:42:44 -0500

    Formatting details in payments-proxy-forward.sh
    Change-Id: I81e7c6b1360ceb1839e4ce397116c2e08a078bb5

--- payments-proxy-forward.sh
+++ payments-proxy-forward.sh
@@ -2,12 +2,18 @@
 
 # Script to set up fundraising development environment
 source .env
+
 if [ -z "$FR_DOCKER_PAYMENTS_TEST_NUMBER" ] || [ -z "$FR_DOCKER_PAYMENTS_HTTP_PORT" ]; then
-  echo "Please run setup.sh and choose a test server number and a payments HTTP port";
-  exit 1
+	echo "Please run setup.sh and choose a test server number and a payments HTTP port";
+	exit 1
 fi;
+
 REMOTE_PORT=$((8000 + FR_DOCKER_PAYMENTS_TEST_NUMBER))
-echo "ssh -o ExitOnForwardFailure=yes -fN -R $REMOTE_PORT:localhost:$FR_DOCKER_PAYMENTS_HTTP_PORT payments.fr-tech-dev"
-ssh -o ExitOnForwardFailure=yes -fN -R $REMOTE_PORT:localhost:$FR_DOCKER_PAYMENTS_HTTP_PORT payments.fr-tech-dev && \
-  echo "https://paymentstest$FR_DOCKER_PAYMENTS_TEST_NUMBER.wmcloud.org should forward to localhost.";
+
+echo "ssh -o ExitOnForwardFailure=yes -fN -R $REMOTE_PORT:localhost:$FR_DOCKER_PAYMENTS_HTTP_PORT"\
+	"payments.fr-tech-dev"
+
+ssh -o ExitOnForwardFailure=yes -fN -R $REMOTE_PORT:localhost:$FR_DOCKER_PAYMENTS_HTTP_PORT \
+	payments.fr-tech-dev && \
+	echo "https://paymentstest$FR_DOCKER_PAYMENTS_TEST_NUMBER.wmcloud.org should forward to localhost.";
 echo
