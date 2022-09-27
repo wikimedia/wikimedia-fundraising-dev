e801671ff01d763192497aaf74456c935a48c3ba
Andrew Green <andrew.green.df@gmail.com>
2022-09-27 10:08:18 -0500

    Memcached purge script
    Change-Id: I967ba8bdc22bfc3b169cf1cf148dae29b18baacd

---
+++ memcached-purge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Clears contents of memcached
+
+# We run this on the payments container for two reasons:
+# - nc is installed there.
+# - It's better not to run on the host machine, since we don't know if nc is
+#   installed locally, and it's easiest to access the memcached server from
+#   within the docker network.
+docker-compose exec -T payments bash -c 'echo flush_all | nc -q 1 memcached 11211'
