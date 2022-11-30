f782eff7e480e90e18a89aff0965b604f96fd5f9
Damilare Adedoyin <dadedoyin@wikimedia.org>
2022-11-30 21:08:01 +0000

    update consume queue order
    Change-Id: I3d7ee7c663dad755dbed0de7bacdcedea62a16d0

--- consume-queues.sh
+++ consume-queues.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
-echo -e "${BLUE}Running pending queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
+
+echo -e "${BLUE}Running anti fraud queue consumer${NC}"
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v afqc
 echo -e "${BLUE}Running contribution tracking queue consumer${NC}"
 docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v ctqc
 echo -e "${BLUE}Running payment init queue consumer${NC}"
 docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v piqc
-echo -e "${BLUE}Running anti fraud queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v afqc
 echo -e "${BLUE}Running donations queue consumer${NC}"
 docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v qc
-echo -e "${BLUE}Running recurring queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v rqc
+echo -e "${BLUE}Running contribution tracking queue consumer${NC}"
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v ctqc
 echo -e "${BLUE}Running refunds queue consumer${NC}"
 docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v rfdqc
+echo -e "${BLUE}Running recurring queue consumer${NC}"
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v rqc
+echo -e "${BLUE}Running pending queue consumer${NC}"
+docker-compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
