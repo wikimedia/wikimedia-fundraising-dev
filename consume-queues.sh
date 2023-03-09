b76e79f3149bce094cf07cab19f894efaf2e4da7
Elliott Eggleston <ejegg@ejegg.com>
2023-03-09 11:41:42 -0500

    Add more queue consumers
    And settings for more queues.
    
    Both settings and consumer for upi-donations (see Ibc09240bffdbd95a12
    and I92e33a41aa624674c01522) and jobs-dlocal (see Ida30895a88934a3df1),
    and consumer for email-preferences.
    
    Also ensure we are running like we do in production by adding @wmff
    and increasing the vebosity to -vv. Note that with -vv we get a big
    annoying 'Box Requirements Checker' block at the start of each drush
    job, but we need that extra verbosity for the \Civi::log() statements
    eillen is replacing watchdog with to show up.
    
    Change-Id: I315c79527d4ac1a05437e84dcc07621c73ac9a03

--- consume-queues.sh
+++ consume-queues.sh
@@ -5,26 +5,32 @@ NC='\033[0m' # No Color
 echo -e "${BLUE}Running pending queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
 echo -e "${BLUE}Running contribution tracking queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v ctqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc
 echo -e "${BLUE}Running anti fraud queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v afqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv afqc
 echo -e "${BLUE}Running payment init queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v piqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv piqc
 echo -e "${BLUE}Running jobs-adyen queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-adyen --config-node adyen
 echo -e "${BLUE}Running jobs-amazon queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-amazon --config-node amazon
+echo -e "${BLUE}Running jobs-dlocal queue consumer${NC}"
+docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-dlocal --config-node dlocal
 echo -e "${BLUE}Running jobs-ingenico queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-ingenico --config-node ingenico
 echo -e "${BLUE}Running jobs-paypal queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-paypal --config-node paypal
 echo -e "${BLUE}Running donations queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v qc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv qc
 echo -e "${BLUE}Running contribution tracking queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v ctqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc
 echo -e "${BLUE}Running refunds queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v rfdqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rfdqc
 echo -e "${BLUE}Running recurring queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush -v rqc
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc
+echo -e "${BLUE}Running upi-donations queue consumer${NC}"
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi UpiDonationsQueue.consume version=4
+echo -e "${BLUE}Running preferences queue consumer${NC}"
+docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Preferencesqueue.consume version=3
 echo -e "${BLUE}Running pending queue consumer${NC}"
 docker-compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
