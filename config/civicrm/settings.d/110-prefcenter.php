e1a33af37ee95a0f1a302dd2644db3fb8703aeab
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-22 17:06:54 +1300

    Specify url for recurring upgrade
    Follow on to https://gerrit.wikimedia.org/r/c/wikimedia/fundraising/crm/+/975105/
    
    Change-Id: If3961eba74018103d67a55157933ee9c8d65a8fc

--- config/civicrm/settings.d/110-prefcenter.php
+++ config/civicrm/settings.d/110-prefcenter.php
@@ -1,4 +1,6 @@
 <?php
 global $civicrm_setting;
 $civicrm_setting['domain']['wmf_email_preferences_url'] = 'https://localhost:' .
-	getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:EmailPreferences/emailPreferences';
+  getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:EmailPreferences/emailPreferences';
+$civicrm_setting['domain']['wmf_recurring_upgrade_url'] = 'https://localhost:' .
+  getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:RecurUpgrade/recurUpgrade';
