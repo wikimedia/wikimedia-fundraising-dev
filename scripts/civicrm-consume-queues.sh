85428dc6e96fa2df4ec23056e6816b33b7fa8ba7
Elliott Eggleston <ejegg@ejegg.com>
2024-02-01 18:33:42 -0500

    Use compose v2 for queue consume script
    Change-Id: Ifd398706f5b241c3c9b73eeb07b0f92a6981cc63

--- scripts/civicrm-consume-queues.sh
+++ scripts/civicrm-consume-queues.sh
@@ -4,32 +4,32 @@ GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Running ${GREEN}pending${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
+docker compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
 echo -e "${BLUE}Running ${GREEN}anti fraud${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv afqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv afqc
 echo -e "${BLUE}Running ${GREEN}payment init${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv piqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv piqc
 echo -e "${BLUE}Running ${GREEN}jobs-adyen${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-adyen --config-node adyen
+docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-adyen --config-node adyen
 echo -e "${BLUE}Running ${GREEN}jobs-amazon${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-amazon --config-node amazon
+docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-amazon --config-node amazon
 echo -e "${BLUE}Running ${GREEN}jobs-dlocal${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-dlocal --config-node dlocal
+docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-dlocal --config-node dlocal
 echo -e "${BLUE}Running ${GREEN}jobs-ingenico${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-ingenico --config-node ingenico
+docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-ingenico --config-node ingenico
 echo -e "${BLUE}Running ${GREEN}jobs-paypal${BLUE} queue consumer${NC}"
-docker-compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-paypal --config-node paypal
+docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-paypal --config-node paypal
 echo -e "${BLUE}Running ${GREEN}donations${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv qc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv qc
 echo -e "${BLUE}Running ${GREEN}contribution tracking${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc
 echo -e "${BLUE}Running ${GREEN}refunds${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rfdqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rfdqc
 echo -e "${BLUE}Running ${GREEN}recurring${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc
 echo -e "${BLUE}Running ${GREEN}recurring-upgrade${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc --queue=recurring-upgrade
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc --queue=recurring-upgrade
 echo -e "${BLUE}Running ${GREEN}upi-donations${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi UpiDonationsQueue.consume version=4
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi UpiDonationsQueue.consume version=4
 echo -e "${BLUE}Running ${GREEN}preferences${BLUE} queue consumer${NC}"
-docker-compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Preferencesqueue.consume version=3
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Preferencesqueue.consume version=3
