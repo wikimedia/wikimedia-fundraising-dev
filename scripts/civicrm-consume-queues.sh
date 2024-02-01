d6901baab17c328293638caf08dd2ce9b4546798
Elliott Eggleston <ejegg@ejegg.com>
2024-02-01 18:45:39 -0500

    Strip some spam off the start of queue consumer logs
    Specifically all the drush preamble about the Box
    prerequisites and such.
    
    Change-Id: Iaf0ab1ded57de1cb65650552756b228d07f7342f

--- scripts/civicrm-consume-queues.sh
+++ scripts/civicrm-consume-queues.sh
@@ -6,9 +6,9 @@ NC='\033[0m' # No Color
 echo -e "${BLUE}Running ${GREEN}pending${BLUE} queue consumer${NC}"
 docker compose exec smashpig php /srv/smashpig/Maintenance/ConsumePendingQueue.php
 echo -e "${BLUE}Running ${GREEN}anti fraud${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv afqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv afqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}payment init${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv piqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv piqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}jobs-adyen${BLUE} queue consumer${NC}"
 docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-adyen --config-node adyen
 echo -e "${BLUE}Running ${GREEN}jobs-amazon${BLUE} queue consumer${NC}"
@@ -20,22 +20,22 @@ docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --
 echo -e "${BLUE}Running ${GREEN}jobs-paypal${BLUE} queue consumer${NC}"
 docker compose exec smashpig php /srv/smashpig/Maintenance/QueueJobRunner.php --queue jobs-paypal --config-node paypal
 echo -e "${BLUE}Running ${GREEN}donations${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv qc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv qc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}contribution tracking${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv ctqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}refunds${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rfdqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rfdqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}recurring${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}recurring-upgrade${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc --queue=recurring-upgrade
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv rqc --queue=recurring-upgrade 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}upi-donations${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi UpiDonationsQueue.consume version=4
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi UpiDonationsQueue.consume version=4 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}preferences${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Preferencesqueue.consume version=3
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Preferencesqueue.consume version=3 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}opt-in${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv oqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv oqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}unsubscribe${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv unsubqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv unsubqc 2>&1 | tail -n +23
 echo -e "${BLUE}Running ${GREEN}banner history${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv bhqc
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv bhqc 2>&1 | tail -n +23
