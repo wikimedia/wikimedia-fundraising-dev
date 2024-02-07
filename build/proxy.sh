bd04b7e15695b6becf4e1e7b297f308f9a37cc5b
Elliott Eggleston <ejegg@ejegg.com>
2024-02-07 10:46:51 -0500

    Skip proxy forward prompt when set, validate
    Only allow a number 1-6, then don't ask again once it's written to
    .env
    
    Change-Id: I4be6e12fe5c7f5a020ac609bdcd3563744f16b3b

--- build/proxy.sh
+++ build/proxy.sh
@@ -1,14 +1,8 @@
 if [ -z "${PROXY_FORWARD_ID}" ]; then
     echo
-    echo "**** Select paymentstest{$}.wmcloud.org testing web address"
-    read -p "Which paymentstest{$}.wmcloud.org numerical URL identifier do you want to use (1-6)?: " PROXY_FORWARD_ID
+    echo "**** Select paymentstest{$}.wmcloud.org testing web address (you can edit .env to change it later)"
+    while ! [[ $PROXY_FORWARD_ID =~ ^[1-6]$ ]]; do
+      read -p "Which paymentstest{$}.wmcloud.org numerical URL identifier do you want to use (1-6)?: " PROXY_FORWARD_ID
+    done
     update_env "PROXY_FORWARD_ID" "${PROXY_FORWARD_ID}"
-else
-    echo
-    echo "**** Test address paymentstest$PROXY_FORWARD_ID.wmcloud.org is currently set"
-    read -rp "Would you like to update it? [y/N]: "
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        read -rp "New paymentstest{$}.wmcloud.org numerical ID: " PROXY_FORWARD_ID
-        update_env "PROXY_FORWARD_ID" "${PROXY_FORWARD_ID}"
-    fi
 fi
