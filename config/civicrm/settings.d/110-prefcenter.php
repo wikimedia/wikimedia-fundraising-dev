fe28be8092dd1e5d06a7af4020908330aec4597c
Elliott Eggleston <ejegg@ejegg.com>
2024-03-13 23:31:48 -0400

    Add wmf_ params to recur upgrade URL, remove subpage
    Also default to the v01 pretty variant
    
    Depends-On: I8c74935c9b7a7014285b89880efc859395d45ef7
    Change-Id: I7886018ed4c4d6b170533fe421c6efeb78bf0df7

--- config/civicrm/settings.d/110-prefcenter.php
+++ config/civicrm/settings.d/110-prefcenter.php
@@ -3,4 +3,5 @@ global $civicrm_setting;
 $civicrm_setting['domain']['wmf_email_preferences_url'] = 'https://localhost:' .
   getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:EmailPreferences/emailPreferences';
 $civicrm_setting['domain']['wmf_recurring_upgrade_url'] = 'https://localhost:' .
-  getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:RecurUpgrade/recurUpgrade';
+  getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:RecurUpgrade' .
+	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource&variant=v01';
