3573ee321e3f36fbdcee93b7ef9805dcd043fd4c
eileen <emcnaughton@wikimedia.org>
2024-04-04 12:55:36 +1300

    Migrate Unsubscribe to use cv
    Bug: T357470
    Change-Id: I84c7352a939a39780203f5957d3328f7b5dfc009

--- scripts/consume-queues-civicrm.sh
+++ scripts/consume-queues-civicrm.sh
@@ -34,7 +34,7 @@ echo -e "${BLUE}Running ${GREEN}opt-in${BLUE} queue consumer${NC}"
 docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm cv api4 --user=admin -vv WMFQueue.Consume timeLimit=280 queueConsumer=OptIn queueName=opt-in 2>&1 | tail -n +18
 
 echo -e "${BLUE}Running ${GREEN}unsubscribe${BLUE} queue consumer${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv unsubqc 2>&1 | tail -n +23
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm cv api4 --user=admin -vv WMFQueue.Consume timeLimit=280 queueConsumer=Unsubscribe queueName=unsubscribe 2>&1 | tail -n +18
 
 echo -e "${BLUE}Running ${GREEN}banner history${BLUE} queue consumer${NC}"
 docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm cv api4 --user=admin -vv WMFQueue.Consume timeLimit=280 queueConsumer=BannerHistory queueName=banner-history 2>&1 | tail -n +18
