8c3d01ac2c816def9a71b198740d3bb189ce8b28
Jack Gleeson <jgleeson@wikimedia.org>
2024-06-17 18:41:31 +0100

    Add script to safely checkout smashpig patch to payments
    Usage: ./scripts/payments-smashpig-checkout-patch.sh GERRIT_PATCH_ID
    
    Sample output: https://phabricator.wikimedia.org/P65113
    
    Bug: T367798
    Change-Id: I837d65689f5cece79f800e0af1613ae8a381c48d

---
+++ scripts/payments-smashpig-restore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "*** Restore original Payments-wiki vendor packages"
+docker compose exec payments bash -c 'if [ -d "/var/www/html/vendor" ]; then rm -rf /var/www/html/vendor; fi'
+docker compose exec -w "/var/www/html/" payments git submodule update --init
+echo "*** Payments-wiki vendor/wikimedia/smash-pig now restored"
