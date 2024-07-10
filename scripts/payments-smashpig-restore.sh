eed08c1ed0a9693b57a39faac56b70839a7620e9
Jack Gleeson <jgleeson@wikimedia.org>
2024-07-10 11:18:03 +0100

    make payments-smashpig-restore.sh only update vendor.
    running `git submodule update --init` was also resetting other
    submodules like DonationInterface. Updated to
    `git submodule update --init vendor` to only target vendor.
    
    Change-Id: I8673ebbb93a2cd9aeabb849d0d6d1a58e50b33bd

--- scripts/payments-smashpig-restore.sh
+++ scripts/payments-smashpig-restore.sh
@@ -2,5 +2,5 @@
 
 echo "*** Restore original Payments-wiki vendor packages"
 docker compose exec payments bash -c 'if [ -d "/var/www/html/vendor" ]; then rm -rf /var/www/html/vendor; fi'
-docker compose exec -w "/var/www/html/" payments git submodule update --init
+docker compose exec -w "/var/www/html/" payments git submodule update --init vendor
 echo "*** Payments-wiki vendor/wikimedia/smash-pig now restored"
