f328ad57a48cb93f4e0b4f2953ddcc91dd529107
Elliott Eggleston <ejegg@ejegg.com>
2024-10-10 20:46:27 -0400

    Update Omnimail settings for upload folder restriction
    Allows uploading from /tmp by default
    
    Change-Id: I6e9beddb127de4ebc3af6fdd949633ecf58fb39c

--- config/civicrm/settings.d/120-acoustic.php
+++ config/civicrm/settings.d/120-acoustic.php
@@ -1,5 +1,8 @@
 <?php
-if ( file_exists ( '/srv/config/private/civicrm/AcousticCredentials.php' ) ) {
-  $civicrm_setting['domain']['omnimail_credentials']['Silverpop'] =
-    require '/srv/config/private/civicrm/AcousticCredentials.php';
+if (file_exists('/srv/config/private/civicrm/AcousticCredentials.php')) {
+	$civicrm_setting['domain']['omnimail_credentials']['Silverpop'] =
+		require '/srv/config/private/civicrm/AcousticCredentials.php';
 }
+$civicrm_setting['domain']['omnimail_allowed_upload_folders'] = [
+	'/tmp/'
+];
