007976e3d2c59700e075bfec7adb58c129436d6d
eileen <emcnaughton@wikimedia.org>
2024-10-18 13:05:40 +1300

    Revert "Update Omnimail settings for upload folder restriction"
    This has been moved to wmf_developer_settings.json
    which implements a default rather than an override - which
    this does (meaning it can be changed by changing the setting
    )
    
    This reverts commit f328ad57a48cb93f4e0b4f2953ddcc91dd529107.
    
    Change-Id: Id243e6ddc25c88cd98d79fbe8af131c508fd1021

--- config/civicrm/settings.d/120-acoustic.php
+++ config/civicrm/settings.d/120-acoustic.php
@@ -1,8 +1,5 @@
 <?php
-if (file_exists('/srv/config/private/civicrm/AcousticCredentials.php')) {
-	$civicrm_setting['domain']['omnimail_credentials']['Silverpop'] =
-		require '/srv/config/private/civicrm/AcousticCredentials.php';
+if ( file_exists ( '/srv/config/private/civicrm/AcousticCredentials.php' ) ) {
+  $civicrm_setting['domain']['omnimail_credentials']['Silverpop'] =
+    require '/srv/config/private/civicrm/AcousticCredentials.php';
 }
-$civicrm_setting['domain']['omnimail_allowed_upload_folders'] = [
-	'/tmp/'
-];
