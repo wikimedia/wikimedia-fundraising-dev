337043e6f38f7d19340aafe45d9f8e194e13e990
Elliott Eggleston <ejegg@ejegg.com>
2025-03-03 21:33:10 -0500

    Remove variant on RecurUpgrade base URL
    Bug: T386693
    Change-Id: If8ee4f3fffa3977fa4e6e0faa11c3128ca7ea9ca

--- config/civicrm/settings.d/110-prefcenter.php
+++ config/civicrm/settings.d/110-prefcenter.php
@@ -4,4 +4,4 @@ $civicrm_setting['domain']['wmf_email_preferences_url'] = 'https://localhost:' .
   getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:EmailPreferences/emailPreferences';
 $civicrm_setting['domain']['wmf_recurring_upgrade_url'] = 'https://localhost:' .
   getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:RecurUpgrade' .
-	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource&variant=v01';
+	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource';
