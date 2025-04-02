ea246cf3da1f2765c945297d4fda7de21e24c308
Jack Gleeson <jgleeson@wikimedia.org>
2025-04-02 15:30:49 -0400

    Update CiviCRM standlone Mac sync scripts
    Change-Id: I1f90d3ac21c15ba9d8fc2b9778922fc62e267f5c

--- scripts/mac/sync-pull-civicrm-standalone.sh
+++ scripts/mac/sync-pull-civicrm-standalone.sh
@@ -2,12 +2,12 @@
 CONTAINER_ID=$(docker compose ps -q civicrm)
 
 # copy remote civicrm standalone source to local
-docker cp -q "${CONTAINER_ID}:/srv/civi-sites/standalone-dev.sh" "./src/civi-sites/standalone-dev.sh"
-echo "container => local: sync complete - ./src/civi-sites/standalone-dev.sh:/srv/civi-sites/standalone-dev.sh"
+docker cp -q "${CONTAINER_ID}:/srv/civi-sites/standalone-clean.sh" "./src/civi-sites/standalone-clean.sh"
+echo "container => local: sync complete - ./src/civi-sites/standalone-clean.sh:/srv/civi-sites/standalone-clean.sh"
 
 TEMP_DIR=$(mktemp -d)
-docker exec -u0 "${CONTAINER_ID}" chown -R ${FR_DOCKER_UID}:${FR_DOCKER_GID} /srv/civi-sites/standalone-dev
-docker cp -q "${CONTAINER_ID}:/srv/civi-sites/standalone-dev" "${TEMP_DIR}"
-cp -rf "${TEMP_DIR}/standalone-dev/." "./src/civi-sites/standalone-dev/"
+docker exec -u0 "${CONTAINER_ID}" chown -R ${FR_DOCKER_UID}:${FR_DOCKER_GID} /srv/civi-sites/standalone-clean
+docker cp -q "${CONTAINER_ID}:/srv/civi-sites/standalone-clean" "${TEMP_DIR}"
+cp -rf "${TEMP_DIR}/standalone-clean/." "./src/civi-sites/standalone-clean/"
 rm -rf "${TEMP_DIR}"
-echo "container => local: sync complete - /srv/civi-sites/standalone-dev:./src/civi-sites/standalone-dev"
+echo "container => local: sync complete - /srv/civi-sites/standalone-clean:./src/civi-sites/standalone-clean"
--- scripts/mac/sync-push-civicrm-standalone.sh
+++ scripts/mac/sync-push-civicrm-standalone.sh
@@ -2,19 +2,19 @@
 CONTAINER_ID=$(docker compose ps -q civicrm)
 
 # copy local civicrm standalone source to container
-if [ -f "./src/civi-sites/standalone-dev.sh" ]; then
-  docker cp -q "./src/civi-sites/standalone-dev.sh" "${CONTAINER_ID}:/srv/civi-sites/standalone-dev.sh"
-  docker exec "${CONTAINER_ID}" chown "${FR_DOCKER_UID}":"${FR_DOCKER_GID}" /srv/civi-sites/standalone-dev.sh
-  echo "local => container: sync complete - ./src/civi-sites/standalone-dev.sh:/srv/civi-sites/standalone-dev.sh"
+if [ -f "./src/civi-sites/standalone-clean.sh" ]; then
+  docker cp -q "./src/civi-sites/standalone-clean.sh" "${CONTAINER_ID}:/srv/civi-sites/standalone-clean.sh"
+  docker exec "${CONTAINER_ID}" chown "${FR_DOCKER_UID}":"${FR_DOCKER_GID}" /srv/civi-sites/standalone-clean.sh
+  echo "local => container: sync complete - ./src/civi-sites/standalone-clean.sh:/srv/civi-sites/standalone-clean.sh"
 fi
 
-if [ -d "./src/civi-sites/standalone-dev" ]; then
-  docker cp -q "./src/civi-sites/standalone-dev" "${CONTAINER_ID}:/tmp/standalone-dev"
-  docker exec -u0 "${CONTAINER_ID}" mkdir -p /srv/civi-sites/standalone-dev
-  docker exec -u0 "${CONTAINER_ID}" chown -R "${FR_DOCKER_UID}":"${FR_DOCKER_GID}" /tmp/standalone-dev /srv/civi-sites/standalone-dev
-  docker exec "${CONTAINER_ID}" cp -rf /tmp/standalone-dev/. /srv/civi-sites/standalone-dev/
-  docker exec "${CONTAINER_ID}" rm -rf /tmp/standalone-dev
-  echo "local => container: sync complete - ./src/civi-sites/standalone-dev:/srv/civi-sites/standalone-dev"
+if [ -d "./src/civi-sites/standalone-clean" ]; then
+  docker cp -q "./src/civi-sites/standalone-clean" "${CONTAINER_ID}:/tmp/standalone-clean"
+  docker exec -u0 "${CONTAINER_ID}" mkdir -p /srv/civi-sites/standalone-clean
+  docker exec -u0 "${CONTAINER_ID}" chown -R "${FR_DOCKER_UID}":"${FR_DOCKER_GID}" /tmp/standalone-clean /srv/civi-sites/standalone-clean
+  docker exec "${CONTAINER_ID}" cp -rf /tmp/standalone-clean/. /srv/civi-sites/standalone-clean/
+  docker exec "${CONTAINER_ID}" rm -rf /tmp/standalone-clean
+  echo "local => container: sync complete - ./src/civi-sites/standalone-clean:/srv/civi-sites/standalone-clean"
 else
-  echo "Directory not found: ./src/civi-sites/standalone-dev"
+  echo "Directory not found: ./src/civi-sites/standalone-clean"
 fi
