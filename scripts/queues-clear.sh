f49d9e8078b7038e5b7ef85168d4b7bb11439473
cstone <cstone@wikimedia.org>
2025-04-09 04:47:14 +0000

    Update references to the new script name
    Change-Id: Ic1c124cc036256466524fbbe4505cbc4a005256d

--- scripts/queues-clear.sh
+++ scripts/queues-clear.sh
@@ -6,15 +6,15 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Get the current sequence number (removing quotes and any trailing newline characters)
 echo "GET sequence_contribution-tracking"
-current_seq=$("${script_dir}/queues-redis-cli.sh" GET sequence_contribution-tracking | tr -d '"\n\r')
+current_seq=$("${script_dir}/queues-cli.sh" GET sequence_contribution-tracking | tr -d '"\n\r')
 echo ${current_seq}
 
 # Clear everything
 echo "FLUSHALL"
-"${script_dir}/queues-redis-cli.sh" FLUSHALL
+"${script_dir}/queues-cli.sh" FLUSHALL
 
 if [ "${current_seq}" != "(nil)" ]; then
   # Restore sequence number
   echo "SET sequence_contribution-tracking ${current_seq}"
-  "${script_dir}/queues-redis-cli.sh" SET sequence_contribution-tracking "${current_seq}"
+  "${script_dir}/queues-cli.sh" SET sequence_contribution-tracking "${current_seq}"
 fi
