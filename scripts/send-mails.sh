f5276acf8b66d158b1b05d4765eb62f54f382fa1
eileen <emcnaughton@wikimedia.org>
2025-04-28 09:47:20 +1200

    Switch thank you to new api, update eoyemail.send
    Still needs some fix for
      Failed to determine contactID for user=root
    
    Bug: T270674
    Change-Id: I1a654278164685474dc6314c6b0c0f540c409364

--- scripts/send-mails.sh
+++ scripts/send-mails.sh
@@ -4,6 +4,6 @@ GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Send ${GREEN}thank you${BLUE} mail${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv thank-you-send --days=1
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm wmf-cv -vv api4 ThankYou.BatchSend numberOfDays=2
 echo -e "${BLUE}Send ${GREEN}eoy${BLUE} mail${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi EOYEmail.Send version=4 limit=5000 timeLimit=840
+docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm wmf-cv -vv api4 EOYEmail.Send limit=5000 timeLimit=840
