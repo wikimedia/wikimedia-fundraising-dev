5233dd6d15f436dea7c28133d66365b8a3dc7b36
Elliott Eggleston <ejegg@ejegg.com>
2025-04-28 15:10:54 -0400

    Set base URL for donor portal in Civi
    For the queue consumer
    
    Bug: T388739
    Change-Id: I1c06cdb276a75d9b174d0d19bb5e33b1be4dbfdd

--- config/civicrm/settings.d/110-prefcenter.php
+++ config/civicrm/settings.d/110-prefcenter.php
@@ -5,3 +5,6 @@ $civicrm_setting['domain']['wmf_email_preferences_url'] = 'https://localhost:' .
 $civicrm_setting['domain']['wmf_recurring_upgrade_url'] = 'https://localhost:' .
   getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:RecurUpgrade' .
 	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource';
+$civicrm_setting['domain']['wmf_donor_portal_url'] = 'https://localhost:' .
+	getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:DonorPortal' .
+	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource';
