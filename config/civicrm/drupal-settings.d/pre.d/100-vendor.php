8cc33ba9895c22562293c0ca6c190acccf5cd57b
eileen <emcnaughton@wikimedia.org>
2025-06-05 11:08:53 +1200

    Remove some fs
    Change-Id: I13d4fc51be165e23e9cc7f1f7a0f026a825dfbab

--- config/civicrm/drupal-settings.d/pre.d/100-vendor.php
+++ config/civicrm/drupal-settings.d/pre.d/100-vendor.php
@@ -2,6 +2,6 @@
 
 global $civibuild;
 // This should confirm it is our local devs....
-if ($civibuild['WEB_ROOT'] === '/srv/civi-sites/wmff') {
-  require_once '/srv/civi-sites/wmff/vendor/autoload.php';
+if ($civibuild['WEB_ROOT'] === '/srv/civi-sites/wmf') {
+  require_once '/srv/civi-sites/wmf/vendor/autoload.php';
 }
