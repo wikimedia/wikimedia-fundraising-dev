79408d638a41d9e10e39c0e199e49677367ee249
eileen <emcnaughton@wikimedia.org>
2025-06-17 16:17:24 +1200

    More drush removal
    Bug: T276297
    Change-Id: I19438bcb9375d191e16b78138e5f0f025a138c8a

--- scripts/omnirecipient-process.sh
+++ scripts/omnirecipient-process.sh
@@ -5,22 +5,22 @@ NC='\033[0m' # No Color
 
 #retrieve mailing data and store to civicrm_mailing_provider_data
 echo -e "${BLUE}Running ${GREEN}Omnirecipient unsubscribe${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv --in=json cvapi Omnirecipient.load
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv --in=json api3 Omnirecipient.load
 #process from civicrm_mailing_provider_data to create unsubscribe activities.
 echo -e "${BLUE}Running ${GREEN}Omnirecipient unsubscribe${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Omnirecipient.process_unsubscribes mail_provider=Silverpop rowCount=5000
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api3 Omnirecipient.process_unsubscribes mail_provider=Silverpop rowCount=5000
 #Manage omnimail job to issue deletion requests for Silverpop.
 echo -e "${BLUE}Running ${GREEN}Omnirecipient forgetme${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Omnirecipient.process_forgetme mail_provider=Silverpop
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api3 Omnirecipient.process_forgetme mail_provider=Silverpop
 #Set on hold for emails we know not to be emailable due to bounce types from Silverpop
 echo -e "${BLUE}Running ${GREEN}Omnirecipient onhold${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Omnirecipient.process_onhold mail_provider=Silverpop rowCount=5000
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api3 Omnirecipient.process_onhold mail_provider=Silverpop rowCount=5000
 #retrieve mailing data and store to tables (a combination of civicrm_campaign, civicrm_mailing and civicrm_mailing_stats)
 echo -e "${BLUE}Running ${GREEN}Omnimailing load${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi Omnimailing.load mail_provider=Silverpop
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api3 Omnimailing.load mail_provider=Silverpop
 #Get group member data from Silverpop in the 'no cid' group
 echo -e "${BLUE}Running ${GREEN}Omnigroupmember load${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv --in=json cvapi Omnigroupmember.load
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv --in=json api3 Omnigroupmember.load
 #match them with mailing data rows that are missing their contact_id because we sent a silverpop mailing before the contact made the round trip to CiviCRM and back out.
 echo -e "${BLUE}Running ${GREEN}Omnigroupmember match${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff cvapi omnigroupmember.match group_id=310 limit=10000
+docker compose exec -w "/srv/civi-sites/wmf" civicrm drush @wmff cvapi omnigroupmember.match group_id=310 limit=10000
--- scripts/send-mails.sh
+++ scripts/send-mails.sh
@@ -4,6 +4,6 @@ GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Send ${GREEN}thank you${BLUE} mail${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm wmf-cv -vv api4 ThankYou.BatchSend numberOfDays=2
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 ThankYou.BatchSend numberOfDays=2
 echo -e "${BLUE}Send ${GREEN}eoy${BLUE} mail${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm wmf-cv -vv api4 EOYEmail.Send limit=5000 timeLimit=840
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 EOYEmail.Send limit=5000 timeLimit=840
