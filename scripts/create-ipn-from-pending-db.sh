b31147235ce2bbbd8620eead77a077fcb02666fe
eileen <emcnaughton@wikimedia.org>
2025-06-17 15:11:37 +1200

    Update local script from drush
    needs wmf-cv update too though...
    
    Bug: T276297
    Change-Id: Ie9e0489510e2f5fd1d14cd6793b7fccc4467ea62

--- scripts/create-ipn-from-pending-db.sh
+++ scripts/create-ipn-from-pending-db.sh
@@ -4,10 +4,10 @@ GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Create IPN from pendingTable for ${GREEN}ingenico${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi PendingTable.consume version=4 gateway=ingenico timeLimit=1500
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 PendingTable.consume gateway=ingenico timeLimit=1500
 echo -e "${BLUE}Create IPN from pendingTable for ${GREEN}adyen${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi PendingTable.consume version=4 gateway=adyen timeLimit=1500
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 PendingTable.consume gateway=adyen timeLimit=1500
 echo -e "${BLUE}Create IPN from pendingTable for ${GREEN}paypal_ec${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi PendingTable.consume version=4 gateway=paypal_ec timeLimit=1500
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 PendingTable.consume gateway=paypal_ec timeLimit=1500
 echo -e "${BLUE}Create IPN from pendingTable for ${GREEN}dlocal${BLUE}${NC}"
-docker compose exec -w "/srv/civi-sites/wmff/drupal" civicrm drush @wmff -vv cvapi PendingTable.consume version=4 gateway=dlocal timeLimit=1500
+docker compose exec -w "/srv/civi-sites/wmf" civicrm wmf-cv -vv api4 PendingTable.consume gateway=dlocal timeLimit=1500
