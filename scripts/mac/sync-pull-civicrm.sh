a39d6b7a373b399a05acfc56b5a764be9ecc863f
wfan <wfan@wikimedia.org>
2025-08-19 17:17:50 -0700

    sync civi config to local
    Change-Id: I6557a1c16125bb2fd53db6ae1f60661cf03c45ed

--- scripts/mac/sync-pull-civicrm.sh
+++ scripts/mac/sync-pull-civicrm.sh
@@ -3,8 +3,10 @@ CONTAINER_ID=civicrm
 
 # copy remote civicrm source to local
 docker compose cp "${CONTAINER_ID}:/srv/civi-sites/wmf.sh" "./src/civi-sites/wmf.sh"
-echo "container => local: sync complete - ./src/civi-sites/wmf.sh:/srv/civi-sites/wmf.sh"
+echo "container => local: sync complete - ./srv/civi-sites/wmf.sh:/src/civi-sites/wmf.sh"
 
-docker compose cp "${CONTAINER_ID}:/srv/civi-sites/wmf/" /srv/civi-sites
-
+docker compose cp "${CONTAINER_ID}:/srv/civi-sites/wmf/" "./src/civi-sites/"
 echo "container => local: sync complete - /srv/civi-sites/wmf:./src/civi-sites/wmf"
+
+docker compose cp "${CONTAINER_ID}:/srv/config/exposed/civicrm/amp/apache.d/" "./config/civicrm/amp/"
+echo "container => local: sync complete - /srv/config/exposed/civicrm/amp/apache.d:./config/civicrm/amp/apache.d"
