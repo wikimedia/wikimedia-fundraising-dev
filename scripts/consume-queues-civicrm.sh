116ebd73dab5996234f503f4d37e1cc875d2f436
wfan <wfan@wikimedia.org>
2025-08-22 11:39:53 -0700

    Add queue for set-primary-email and a consume
    Bug: T396523
    Change-Id: I403036867108b1c9ccfc8eda28b70083731056bc

--- scripts/consume-queues-civicrm.sh
+++ scripts/consume-queues-civicrm.sh
@@ -38,3 +38,6 @@ docker compose exec -w "/srv/civi-sites/wmf" civicrm cv api4 --user=admin -vv WM
 
 echo -e "${BLUE}Running ${GREEN}new-checksum-link${BLUE} queue consumer${NC}"
 docker compose exec -w "/srv/civi-sites/wmf" civicrm cv api4 --user=admin -vv WMFQueue.Consume timeLimit=280 queueConsumer=NewChecksumLink queueName=new-checksum-link 2>&1 | tail -n +18
+
+echo -e "${BLUE}Running ${GREEN}set-primary-email${BLUE} queue consumer${NC}"
+docker compose exec -w "/srv/civi-sites/wmf" civicrm cv api4 --user=admin -vv WMFQueue.Consume timeLimit=280 queueConsumer=SetPrimaryEmail queueName=set-primary-email 2>&1 | tail -n +18
