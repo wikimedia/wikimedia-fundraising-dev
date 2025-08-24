33f651e4738c27b26a990f2c8f1991fa5d814003
wfan <wfan@wikimedia.org>
2025-08-24 13:58:25 -0700

    Set base URL for set primary email in Civi
    For the queue consumer
    
    Bug: T396523
    Change-Id: I4b444030ca42f02a99c65e6c9542ee92423f2001

--- config/civicrm/settings.d/110-prefcenter.php
+++ config/civicrm/settings.d/110-prefcenter.php
@@ -8,3 +8,5 @@ $civicrm_setting['domain']['wmf_recurring_upgrade_url'] = 'https://localhost:' .
 $civicrm_setting['domain']['wmf_donor_portal_url'] = 'https://localhost:' .
 	getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:DonorPortal' .
 	'&wmf_campaign=testCampaign&wmf_medium=testMedium&wmf_source=testSource';
+$civicrm_setting['domain']['wmf_confirm_primary_email_url'] = 'https://localhost:' .
+    getenv( 'FR_DOCKER_EMAIL_PREF_CTR_PORT' ) . '/index.php?title=Special:EmailPreferences/confirmEmail';
