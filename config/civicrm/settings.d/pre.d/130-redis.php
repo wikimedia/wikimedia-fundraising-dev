d7bfa12896eccc2eee317d3f041f6223d09e77c1
Elliott Eggleston <ejegg@ejegg.com>
2025-09-09 09:58:23 -0400

    One more redis session save fix
    Change-Id: I2dd5feff33791f6c9d8767e69d78a53781da394c

--- config/civicrm/settings.d/pre.d/130-redis.php
+++ config/civicrm/settings.d/pre.d/130-redis.php
@@ -4,5 +4,6 @@ global $civibuild;
 // This should confirm it is our local devs....
 if (str_starts_with($civibuild['WEB_ROOT'], '/srv/civi-sites/wmf')) {
   define('CIVICRM_DB_CACHE_HOST', 'queues');
-  define('CIVICRM_DB_CACHE_CLASS', 'redis');
+  define('CIVICRM_DB_CACHE_CLASS', 'Redis');
+  ini_set('session.save_handler', 'redis');
 }
