ae47fc14ab9d7f8ab599d1b930043fbdca9a2b6a
Jack Gleeson <jgleeson@wikimedia.org>
2026-03-03 14:14:15 +0000

    Allow test suite to be passed when running python tools unit tests
    Example: ./scripts/tools-tox.sh mediawiki_donor_export/tests/test_export.py
    
    Bug: T416948
    Change-Id: Ic5b24229097e2f1525543eb57e904a6cfc2236e8

--- scripts/tools-tox.sh
+++ scripts/tools-tox.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [ $# -gt 0 ]; then
+    TOX_ARGS="-e discover -- $*"
+else
+    TOX_ARGS=""
+fi
+
 docker compose exec -w "/srv/tools" -u root tools sh -c "
     # Run tox, and exit if it fails
-    tox -r &&
+    tox -r $TOX_ARGS &&
     # If tox was successful, fix cache permissions
     chown -R \"\$FR_DOCKER_UID\":\"\$FR_DOCKER_GID\" /srv/tools
 "
